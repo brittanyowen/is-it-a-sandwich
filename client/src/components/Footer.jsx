d8135b33e9c55becb99a6d81fa13e571b848794f
Brittany Owen <owen.brittany@gmail.com>
2021-02-22 13:35:31 -0600

    favicon added and sources added to footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,6 +4,8 @@ import "./Footer.css"
 const Footer = () => {
   return (
     <div className="footer">
+        &copy; Brittany Owen
+      <div className="links">
       <a href="https://github.com/brittanyowen" target="_blank">
         <img
           src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/github_fpykxh.png"
@@ -11,14 +13,16 @@ const Footer = () => {
           rel="no referrer"
           />
       </a>
-          &copy; Brittany Owen
       <a href="https://www.linkedin.com/in/bnowen/" target="_blank">
         <img
           src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/linkedin_vnvo6s.png"
           alt="LinkedIn Logo"
           rel="no referrer"
-        />
+          />
       </a>
+          </div>
+      <div className="icon">Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> <br></br>
+      Images from <a href="https://www.unsplash.com" title="Unsplash">www.unsplash.com</a></div>
     </div>
   );
 };
