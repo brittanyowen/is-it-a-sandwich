5ae31a50e02e454a98b812ac8b3291b0fba28572
Brittany Owen <owen.brittany@gmail.com>
2021-02-22 16:35:30 -0600

    added pie chart animation onload

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -1,34 +1,40 @@
 import React from "react";
-import {PieChart} from 'react-minimal-pie-chart'
-import "./Results.css"
+import { PieChart } from "react-minimal-pie-chart";
+import "./Results.css";
+
+// https://github.com/toomuchdesign/react-minimal-pie-chart
 
 const Results = (props) => {
   const { food, yesVotes, noVotes } = props.food.fields;
-  const yes = parseInt(`${yesVotes}`); 
-  const no = parseInt(`${noVotes}`)
-  const totalVotes = yes + parseInt(`${noVotes}`); 
-  const percentYes = Math.round((yes / totalVotes) * 100); 
+  const yes = parseInt(`${yesVotes}`);
+  const no = parseInt(`${noVotes}`);
 
   return (
     <div>
-    <div id={props.food.id}>
-      <h3 className="percentage-header">{food === "poptart" ? "poptart (is a ravioli)" : food}</h3>
-      <PieChart
-        key={`${props.food.id}`}
-        data={[
-          { title: 'YES', value: yes, color: '#8fcb9b' },
-          { title: 'NO', value: no, color: '#5b9279' }
-        ]}
-        label={({ dataEntry }) => `${Math.round(dataEntry.percentage)}% ${dataEntry.title} `}
-        labelStyle={() => ({
-          fontSize: '8px',
-        })}
-        radius={40}
-        center={[50, 40]}
-        viewBoxSize={[100, 90]}
+      <div id={props.food.id}>
+        <h3 className="percentage-header">
+          {food === "poptart" ? "poptart (is a ravioli)" : food}
+        </h3>
+        <PieChart
+          key={`${props.food.id}`}
+          data={[
+            { title: "YES", value: yes, color: "#8fcb9b" },
+            { title: "NO", value: no, color: "#5b9279" },
+          ]}
+          label={({ dataEntry }) =>
+            `${Math.round(dataEntry.percentage)}% ${dataEntry.title} `
+          }
+          labelStyle={{
+            fontSize: "8px",
+          }}
+          radius={40}
+          center={[50, 40]}
+          viewBoxSize={[100, 90]}
+          animate={true}
+          animationDuration={1000}
         />
+      </div>
     </div>
-        </div>
   );
 };
 export default Results;
